9c63070c344943b6692f2cbe0e4a82696882ea14
mundane <799699348@qq.com>
2025-04-10 21:26:39 +0800

    构建时忽略eslint错误

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   /* config options here */
+  eslint: {
+    // 忽略构建时的 eslint 错误
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
